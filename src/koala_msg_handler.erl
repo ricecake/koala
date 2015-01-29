6a49b213bc435e221f83635a9a999f4e1b918e56
Sebastian Green-Husted <ricecake@tfm.nu>
2015-01-28 20:43:43 -0500

    Update koala_msg_handler.erl

--- src/koala_msg_handler.erl
+++ src/koala_msg_handler.erl
@@ -5,15 +5,17 @@
 -export([websocket_info/3]).
 
 init(Req, _Opts) ->
-        io:format("StartedA\n"),
 	{cowboy_websocket, Req, #{}}.
 
-websocket_handle({_Type, JSON} = Data, Req, State) ->
-        io:format("Got: ~p~n", [Data]),
-	ok = handle_client_task(JSON, State),
-	{ok, Req, State}.
+websocket_handle({text, JSON} = Data, Req, State) ->
+	Message = jiffy:decode(JSON, [return_maps]),
+	case handle_client_task(JSON, State) of
+		{reply, Data, NewState} when is_list(Data) -> {reply, [{text, Item} || Item <- Data], Req, NewState};
+		{reply, Data, NewState} -> {reply, {text, Data}, Req, NewState};
+		{ok, NewState} -> {ok, Req, NewState}
+	end.
 
 websocket_info(Message, Req, State) ->
 	{reply, Message, Req, State}.
 
-handle_client_task(_JSON, _State) -> ok.
+handle_client_task(_JSON, State) -> {ok, State}.
