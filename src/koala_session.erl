34e28327d43db409f3bc36d5e39c9eeaaa086123
Sebastian Green-Husted <ricecake@tfm.nu>
2015-02-03 20:52:17 -0500

    Update koala_session.erl

--- src/koala_session.erl
+++ src/koala_session.erl
@@ -1,50 +1,26 @@
 -module(koala_session).
--behaviour(gen_server).
--define(SERVER, ?MODULE).
 
-%% ------------------------------------------------------------------
-%% API Function Exports
-%% ------------------------------------------------------------------
+-behaviour(supervisor).
 
+%% API
 -export([start_link/0]).
 
-%% ------------------------------------------------------------------
-%% gen_server Function Exports
-%% ------------------------------------------------------------------
+%% Supervisor callbacks
+-export([init/1]).
 
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Args), {I, {I, start_link, Args}, permanent, 5000, Type, [I]}).
 
-%% ------------------------------------------------------------------
-%% API Function Definitions
-%% ------------------------------------------------------------------
+%% ===================================================================
+%% API functions
+%% ===================================================================
 
 start_link() ->
-    gen_server:start_link(?MODULE, [], []).
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-%% ------------------------------------------------------------------
-%% gen_server Function Definitions
-%% ------------------------------------------------------------------
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
 
-init(Args) ->
-    {ok, Args}.
-
-handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
-
-handle_cast(_Msg, State) ->
-    {noreply, State}.
-
-handle_info(_Info, State) ->
-    {noreply, State}.
-
-terminate(_Reason, _State) ->
-    ok.
-
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
-%% ------------------------------------------------------------------
-%% Internal Function Definitions
-%% ------------------------------------------------------------------
-
+init([]) ->
+    {ok, { {simple_one_for_one, 5, 10}, [?CHILD(koala_session)]} }.
