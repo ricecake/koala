b80329cb8aee1abaf84129ccf7b8e0c023894b4c
Sebastian Green-Husted <ricecake@tfm.nu>
2015-02-24 19:46:08 -0500

    Update koala_msg_handler.erl

--- src/koala_msg_handler.erl
+++ src/koala_msg_handler.erl
@@ -10,7 +10,7 @@ init(Req, _Opts) ->
 
 websocket_handle({text, JSON} = Data, Req, State) ->
 	Message = jiffy:decode(JSON, [return_maps]),
-	case handle_client_task(JSON, State) of
+	case handle_client_task(Message, State) of
 		{reply, Data, NewState} when is_list(Data) -> {reply, [{text, Item} || Item <- Data], Req, NewState};
 		{reply, Data, NewState} -> {reply, {text, Data}, Req, NewState};
 		{ok, NewState} -> {ok, Req, NewState}
